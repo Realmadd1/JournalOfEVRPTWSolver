1ef25e3e81682b5f62ec0715b43bbe627e27dafb
add1 <1206841441@qq.com>
2024-06-10 15:44:15 +0800

    feat:新增部分变量

--- CPModel/AlternativeResourceModel.py
+++ CPModel/AlternativeResourceModel.py
@@ -5,6 +5,10 @@ from docplex.cp.function import *
 class AlternativeResourceModel:
     def __init__(self, data):
         self.data = data
+        self.model = CpoModel("AlternativeResourceModel")
+        self.xm = {}            # 强制区间变量, xm[i], 客户i的服务
+        self.xo = {}            # 可选区间变量, xo[i,k], 客户i与车辆k的关联
+        self.pai = {}           # 序列变量, pai[k], 与车辆k相关的变量集合
 
     def run(self):
         self.__createVariables()
@@ -13,7 +17,7 @@ class AlternativeResourceModel:
         self.__solve()
 
     def __createVariables(self):
-        pass
+        self.pai = self.model.sequence_var(size=5, types=[1, 2, 3, 4, 5])
 
     def __createObjection(self):
         pass
